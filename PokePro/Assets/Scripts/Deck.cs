56c5454feff80a9a9d3a31e99a0de7df36fea5db
PlayStudy <playstudy00@gmail.com>
2023-03-05 21:57:36 +0900

    Create deck
    Create Deck with DeckName

--- PokePro/Assets/Scripts/Deck.cs
+++ PokePro/Assets/Scripts/Deck.cs
@@ -1,15 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class Deck : MonoBehaviour
 {
-    public List<Card> deckList{get; set;}
-    public string deckName{get; set;}
+    public List<Card> deckList;
+    public string deckName;
+
+    public Deck()
+    {
+        deckName = "TestDeck1";
+    }
+    
+    [System.Serializable]
+    public class DeckMaterial
+    {
+        public string[] cardCodeList;
+
+        public List<Card> Parse()
+        {
+            List<Card> list = new List<Card>();
+            
+            foreach(string cardCode in cardCodeList)
+            {
+                
+                TextAsset text = Resources.Load<TextAsset>("CardText/" + cardCode);
+                CardMaterial cardMaterial = JsonUtility.FromJson<CardMaterial>(text.text);
+                list.Add(cardMaterial.Parse());
+            }
+            return list;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
+        TextAsset text = Resources.Load<TextAsset>("Deck/" + deckName);
         
+        DeckMaterial deckMaterial = JsonUtility.FromJson<DeckMaterial>(text.text);
+        this.deckList = deckMaterial.Parse();
+        foreach(Card c in deckList)
+        {
+            Debug.Log(c.cardNote);
+        }
     }
 
     // Update is called once per frame
@@ -20,10 +54,15 @@ public class Deck : MonoBehaviour
     public void Shuffle()
     {
         for (int p = 0; p < deckList.Count; p++)
-            Swap(p, Random.Range(0, deckList.Count));
+            Swap(p, UnityEngine.Random.Range(0, deckList.Count));
     }
     private void Swap(int a, int b)
     {
         Card c = deckList[b]; deckList[b] = deckList[a]; deckList[a] = c;
     }
+    public void AddCard(Card c)
+    {
+        deckList.Add(c);
+    }
 }
+
--- PokePro/Assets/Scripts/MonsterCard.cs
+++ PokePro/Assets/Scripts/MonsterCard.cs
@@ -1,38 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[CreateAssetMenu(fileName = "MonsterCard", menuName = "Scriptable Object/MonsterCard", order = 3)]
 public class MonsterCard: Card
 {
-
-    [SerializeField]
-    private int hp;
-    [SerializeField]
-    private string baseMonster;
-    [SerializeField]
-    private Evolved evolved;
-    [SerializeField]
-    private ElementType type;
-    [SerializeField]
-    private ElementType weakness;
-    [SerializeField]
-    private ElementType resistance;
-    [SerializeField]
-    private int retreatCost;
-
-    public MonsterCard(string card_Code) : base(card_Code)
-    {
-        string filePath = "JSon/";
-
-        TextAsset cardInfo = Resources.Load<TextAsset>(filePath + cardCode);
-
-        this.baseMonster = cardInfo.baseMonster;
-        this.evolved = ReturnEvolve(cardInfo.evolved);
-        this.hp = Int32.Parse(cardInfo.hp);
-        this.type = ReturnElement(cardInfo[5]);
-        this.effects = MakeEffectList(card_Code);
-        this.weakness = ReturnElement(cardInfo[7]);
-        this.resistance = ReturnElement(cardInfo[8]);
-        this.retreatCost = Int32.Parse(cardInfo[9]);
-    }
+    public int hp{get; set;}
+    public string baseMonster{get; set;}
+    public Evolved evolved{get; set;}
+    public ElementType type{get; set;}
+    public ElementType weakness{get; set;}
+    public ElementType resistance{get; set;}
+    public int retreatCost{get; set;}
+    public Rule rule{get; set;}
 }
